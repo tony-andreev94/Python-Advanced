2b1740fa81b19617db0cdd8aa8afff1a6ec8e97f
Tony <tony.andreev94@outlook.com>
2020-06-11 19:11:03 +0300

    # Some comprehension tasks added.

--- 04. Comprehension/Exercise/02. Words Lengths.py
+++ 04. Comprehension/Exercise/02. Words Lengths.py
@@ -1,6 +1,12 @@
 # https://judge.softuni.bg/Contests/Compete/Index/1837#1
 
+# list comprehension
 result_list = [f"{each} -> {len(each)}" for each in input().split(", ")]
+print(", ".join(result_list))
+
+# dictionary comprehension
+input_elements = {x: len(x) for x in input().split(", ")}
+result_list = [f"{key} -> {len(value)}" for key, value in input_elements.items()]
 print(", ".join(result_list))
 
 # no comprehension
